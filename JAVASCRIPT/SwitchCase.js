d6c660f5d81b1ef743e266625c65beb008308fe6
MAYANK PAL <145184894+programmer-mAYANK@users.noreply.github.com>
2025-10-10 17:49:52 +0530

    SwitchCase

---
+++ JAVASCRIPT/SwitchCase.js
@@ -0,0 +1,15 @@
+let fruit = "apple";
+switch (fruit) {
+    case "apple":
+        console.log("you like apple")
+        break;
+        case "orange":
+        console.log("you like orange")
+        case "cheery":
+        console.log("you like cheery")
+        break;
+
+    default:
+        console.log("you dont like any of these fruits.")
+        break;
+}
