3b464cec65b7b9e22b5c88bb5c332032a76cfc13
MAYANK PAL <145184894+programmer-mAYANK@users.noreply.github.com>
2025-10-16 15:43:41 +0530

    Operator.js

---
+++ JAVASCRIPT/Operators.js
@@ -0,0 +1,52 @@
+// OPERTAORS:
+// Arithmetic Operators: + - * / % ++ 
+// Comparison Operators: == < > <= >= != ===
+// Assignment Operators: = += -= *= /= %=
+// sum -= num;
+// sum = sum - num;
+// Logical Operators: && || !
+// first codition && 2nd condition
+// 5 > 4 && 4 > 2(true), 5 > 4 && 4 > 6 (false)
+// first codition && 2nd condition
+// 5 > 4 || 4 > 2 (true), 5 > 4 || 4 > 6 (true), 1 > 4 || 4 > 6 (false)
+
+
+// let num1 = 4;
+// let num2 = 2;
+// let addition = num1 + num2;
+// let sub = num1 + num2;
+// let multiplication = num1 + num2;
+// let division = num1 + num2;
+// console.log("Addition: " + addition);
+// console.log("Subtraction: " + sub);
+// console.log("Multiplication: " + multiplication);
+// console.log("Division: " + division);
+// console.log("Remainder: " + mod);
+// console.log("Exp: " + exp);
+// console.log(num1 + num2)
+
+// console.log(num1 == num2);
+// console.log(num1 < num2);
+// console.log(num1 > num2);
+// console.log(num1 <= num2);
+// console.log(num1 >= num2);
+
+// *** diff b/w = == ===
+// = value will assign
+// == value will compare/check
+// === value and data-types both will check/compare
+
+// console.log(5 == 5); // true=
+// console.log(5 == "5"); // true
+// console.log(5 === 5); // true
+// console.log(5 === "5"); // false
+
+//let Num1 = 10;
+// let Num2 = 20;
+// let result = Num1 += Num2;
+// console.log("result will be: " + result); 
+
+// user input: prompt()
+let num1 = ParseIntprompt("Enter 1st number");
+let num2 = ParseIntprompt("Enter 2nd number");    
+console.log(num1 + num2)
