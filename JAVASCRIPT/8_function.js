0b7ddd90abfd04cb4f11ea34a62994cb99794105
MAYANK PAL <145184894+programmer-mAYANK@users.noreply.github.com>
2025-10-16 15:36:04 +0530

    8_function.js

---
+++ JAVASCRIPT/8_function.js
@@ -0,0 +1,59 @@
+// i. function declaration :
+// function greet() {
+    // block of code to be executed
+//    console.log("Hello World")
+// }
+// greet(); // function calling
+// greet();
+
+// sum of two numbers using functuion
+
+// function sum(num1, num2){ // parameter
+//    console.log(num1 + num2)
+
+// }
+// sum(20, 30);
+
+//
+// greet();
+
+// function greet(){
+//     console.log("Hello User..")
+// }
+
+// num = 30; // expression
+// console.log(num);
+// var num;
+
+//
+// var num
+// console.log(num);
+// var num;
+// num = 30;
+
+//
+// var greet = function () {
+//     console.log("Hello User");
+// }
+// greet();
+
+//
+// greet();
+// let greet = function () {
+//    console.log("Hello,User")
+// }
+//
+// function greet(name = "User") {
+//    console. log("Hello, " + name)
+// }
+// greet("John")
+// greet()
+
+// reverse a string using function - "hello" -> "olleh"
+// reverse() - array method
+
+function reverseString (str) {
+    return str.split(''), reverse() .join('');
+}
+let result = reverseString ("hello");
+console.log(result);
