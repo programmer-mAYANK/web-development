0346d7e6014bbde1d594adaa442c727c425e72b1
MAYANK PAL <145184894+programmer-mAYANK@users.noreply.github.com>
2025-10-16 15:41:45 +0530

    For-loop.js

---
+++ JAVASCRIPT/For-loop.js
@@ -0,0 +1,14 @@
+// Guess a number..
+// secretnumber = 50;
+// without if-else 
+
+let secretNumber = 5;
+let guess = prompt("Guess a number between 1-10")
+while(guess != secretNumber){
+    guess = parseInt(prompt("wrong guess, Try again"))
+}
+alert("You guessed the number")
+
+// if-else
+
+// For Loop: A for loop is used to execute a block of code a certain number of times.
